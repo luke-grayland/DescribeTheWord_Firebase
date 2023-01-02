cafb0813e132e98679b210aed819a5f46912f4d3
Luke Grayland <lgrayland96@gmail.com>
2023-01-02 15:21:34 +0000

    added functionality for random category

--- functions/Controllers/WordsController.js
+++ functions/Controllers/WordsController.js
@@ -3,9 +3,13 @@ const {JsonResponse} = require("../Services/JsonResponseService");
 
 module.exports.GetWordsByCategory = async (req, res) => {
     const db = admin.firestore()
+    const category = req.params.category.toString().toLowerCase()
     const allWords = await db.collection("Words").get()
-    const words = allWords.docs
-        .filter(doc => doc.data().Category.toLowerCase() === req.params.category.toString())
+
+    const words = category === 'random'
+        ? allWords.docs.map(doc => doc.data().Word)
+        : allWords.docs
+        .filter(doc => doc.data().Category.toLowerCase() === category)
         .map(doc => doc.data().Word)
 
     const response = words.length > 0
